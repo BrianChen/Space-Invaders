2f508b83ca04a5a489af92afb9e0f1e6208fc75b
brian <brianc.cb@gmail.com>
2016-11-18 11:09:59 -0800

    added scoring and lives

--- bundle.js
+++ bundle.js
@@ -125,7 +125,7 @@
 	      this.alien = new _alien2.default(this.stage, this.canvas);
 	      this.alien.draw(4);
 	      this.spaceship.draw();
-	      createjs.Ticker.setFPS(3);
+	      createjs.Ticker.setFPS(40);
 	      createjs.Ticker.on("tick", function () {
 	        return _this2.handleTick();
 	      });
@@ -137,7 +137,14 @@
 	    value: function handleTick() {
 	      if (!createjs.Ticker.getPaused()) {
 	        this.alien.moveAliens();
-	        _bullet2.default.moveBullets(this.stage);
+	        _bullet2.default.moveBullets(this.stage, this.spaceship);
+	        _bullet2.default.moveAlienBullets(this.stage, this.alien);
+	        _bullet2.default.checkHits(this.stage, this.spaceship);
+	        _bullet2.default.checkIfDamaged(this.stage, this.alien);
+	        var randomNum = Math.floor(Math.random() * 50) + 1;
+	        if (randomNum == 5) {
+	          this.alien.fireAlienBullets();
+	        }
 	        this.stage.update();
 	      }
 	    }
@@ -148,7 +155,6 @@
 	      createjs.Ticker.setPaused(paused);
 	      var pausedValue = paused ? "unpause" : "pause";
 	      $('.pause-button').attr('value', pausedValue);
-	      debugger;
 	    }
 	  }, {
 	    key: 'keyDown',
@@ -219,10 +225,14 @@
 	
 	      var img = new Image();
 	      img.src = 'assets/spaceship.png';
+	      var x = this.x;
+	      var y = this.y;
 	      img.onload = function (event) {
 	        var bitmap = new createjs.Bitmap('assets/spaceship.png');
-	        bitmap.x = _this.x;
-	        bitmap.y = _this.y;
+	        bitmap.x = x;
+	        bitmap.y = y;
+	        bitmap.height = 20;
+	        bitmap.width = 20;
 	        bitmap.name = "spaceship";
 	        _this.stage.addChild(bitmap);
 	        _this.stage.update();
@@ -235,13 +245,13 @@
 	      if (keycode == 37) {
 	        //left 37
 	        if (spaceship.x > 40) {
-	          spaceship.x -= 30;
+	          spaceship.x -= 10;
 	          this.x = spaceship.x;
 	        }
 	      } else if (keycode == 39) {
 	        //right 39
 	        if (spaceship.x + 30 < canvas.width - 40) {
-	          spaceship.x += 30;
+	          spaceship.x += 10;
 	          this.x = spaceship.x;
 	        }
 	      }
@@ -250,9 +260,10 @@
 	  }, {
 	    key: 'fire',
 	    value: function fire() {
-	      var bullet = new _bullet2.default(this.stage, this.x, this.y);
+	      var bullet = _bullet2.default.drawSpaceshipBullet(this.x, this.y);
 	      this.bullets.push(bullet);
-	      bullet.draw();
+	      this.stage.addChild(bullet);
+	      this.stage.update();
 	    }
 	  }]);
 	
@@ -263,7 +274,7 @@
 
 /***/ },
 /* 3 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 	
@@ -273,6 +284,12 @@
 	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
+	var _bullet = __webpack_require__(4);
+	
+	var _bullet2 = _interopRequireDefault(_bullet);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 	
 	var Alien = function () {
@@ -281,12 +298,14 @@
 	
 	    this.stage = stage;
 	    this.right = true;
+	    this.bullets = [];
 	  }
 	
 	  _createClass(Alien, [{
 	    key: 'draw',
 	    value: function draw(rows) {
 	      this.aliens = new createjs.Container();
+	      this.aliens.name = "alienContainer";
 	
 	      for (var i = 0; i < rows; i++) {
 	        for (var j = 0; j < 8; j++) {
@@ -297,6 +316,8 @@
 	          var bitmap = new createjs.Bitmap('assets/alien.png');
 	          bitmap.x = x;
 	          bitmap.y = y;
+	          bitmap.width = 30;
+	          bitmap.height = 5;
 	          this.aliens.addChild(bitmap);
 	        }
 	      }
@@ -310,18 +331,26 @@
 	      if (this.right) {
 	        if (this.aliens.x + 160 > canvas.width - 30) {
 	          this.right = !this.right;
-	          this.aliens.y += 10;
+	          this.aliens.y += 5;
 	        } else {
-	          this.aliens.x += 20;
+	          this.aliens.x += 1;
 	        }
 	      } else {
 	        if (this.aliens.x < 20) {
 	          this.right = !this.right;
-	          this.aliens.y += 10;
+	          this.aliens.y += 5;
 	        } else {
-	          this.aliens.x -= 20;
+	          this.aliens.x -= 1;
 	        }
 	      }
+	    }
+	  }, {
+	    key: 'fireAlienBullets',
+	    value: function fireAlienBullets() {
+	      var bullet = _bullet2.default.drawAlienBullet(this.stage);
+	      this.bullets.push(bullet);
+	      this.stage.addChild(bullet);
+	      this.stage.update();
 	    }
 	  }]);
 	
@@ -332,7 +361,7 @@
 
 /***/ },
 /* 4 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 	
@@ -342,41 +371,112 @@
 	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
+	var _helper = __webpack_require__(6);
+	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 	
 	var Bullet = function () {
-	  function Bullet(stage, x, y) {
+	  function Bullet(stage) {
 	    _classCallCheck(this, Bullet);
 	
 	    this.stage = stage;
-	    this.x = x;
-	    this.y = y;
 	  }
 	
-	  _createClass(Bullet, [{
-	    key: 'draw',
-	    value: function draw() {
-	      var bulletContainer = void 0;
-	      if (this.stage.getChildByName('bulletContainer')) {
-	        bulletContainer = this.stage.getChildByName('bulletContainer');
-	      } else {
-	        bulletContainer = new createjs.Container();
-	        bulletContainer.name = 'bulletContainer';
-	      }
-	
-	      var bulletShape = new createjs.Shape();
-	      bulletShape.graphics.beginFill("yellow").drawRect(this.x + 15, this.y, 2, 5);
-	      bulletContainer.addChild(bulletShape);
-	      this.stage.addChild(bulletContainer);
-	      this.stage.update();
+	  _createClass(Bullet, null, [{
+	    key: 'drawSpaceshipBullet',
+	    value: function drawSpaceshipBullet(x, y) {
+	      var bullet = new createjs.Shape();
+	      bullet.graphics.beginFill("yellow").drawRect(0, 0, 2, 5);
+	      bullet.x = x + 15;
+	      bullet.y = y;
+	      bullet.width = 2;
+	      bullet.height = 5;
+	      return bullet;
 	    }
-	  }], [{
+	  }, {
+	    key: 'drawAlienBullet',
+	    value: function drawAlienBullet(stage) {
+	      var aliens = stage.getChildByName('alienContainer').children;
+	      var randNum = Math.floor(Math.random() * aliens.length) + 1;
+	      var x = aliens[randNum].localToGlobal(0, 0).x + aliens[randNum].width / 2;
+	      var y = aliens[randNum].localToGlobal(0, 0).y + aliens[randNum].height;
+	
+	      var bullet = new createjs.Shape();
+	      bullet.graphics.beginFill("red").drawRect(0, 0, 2, 3);
+	      bullet.x = x;
+	      bullet.y = y;
+	      bullet.width = 2;
+	      bullet.height = 3;
+	      return bullet;
+	    }
+	  }, {
 	    key: 'moveBullets',
-	    value: function moveBullets(stage) {
-	      if (stage.getChildByName('bulletContainer')) {
-	        var bullets = stage.getChildByName('bulletContainer').children;
+	    value: function moveBullets(stage, spaceship) {
+	      var bullets = spaceship.bullets;
+	      for (var i = 0; i < bullets.length; i++) {
+	        bullets[i].y -= 3;
+	        if (bullets[i].y < 0) {
+	          spaceship.bullets.splice(i, 1);
+	          stage.removeChild(bullets[i]);
+	        }
+	      }
+	    }
+	  }, {
+	    key: 'moveAlienBullets',
+	    value: function moveAlienBullets(stage, alien) {
+	      var bullets = alien.bullets;
+	      var deleteBulletIndexs = [];
+	      for (var i = 0; i < bullets.length; i++) {
+	        bullets[i].y += 1;
+	        if (bullets[i].y > canvas.height) {
+	          deleteBulletIndexs.push(i);
+	        }
+	      }
+	      // for (let i = 0; i < deleteBulletIndexs.length; i++){
+	      //   alien.bullets.splice(i,1);
+	      //   stage.removeChild(bullets[i]);
+	      // }
+	    }
+	  }, {
+	    key: 'checkHits',
+	    value: function checkHits(stage, spaceship) {
+	      var alienContainer = stage.getChildByName('alienContainer');
+	      var bullets = spaceship.bullets;
+	      if (alienContainer && bullets.length > 0) {
+	        var aliens = alienContainer.children;
+	        if (aliens.length == 0) {
+	          // newWave();
+	        } else {
+	          for (var i = 0; i < bullets.length; i++) {
+	            for (var j = 0; j < aliens.length; j++) {
+	              var bullet = bullets[i];
+	              var alien = aliens[j];
+	              if (bullet.y <= alien.localToGlobal(0, 0).y + alien.height && bullet.x <= alien.localToGlobal(0, 0).x + alien.width && bullet.x >= alien.localToGlobal(0, 0).x) {
+	                (0, _helper.incrementScore)();
+	                spaceship.bullets.splice(i, 1);
+	                alienContainer.removeChild(alien);
+	                stage.removeChild(bullet);
+	              }
+	            }
+	          }
+	        }
+	      }
+	    }
+	  }, {
+	    key: 'checkIfDamaged',
+	    value: function checkIfDamaged(stage, alien) {
+	      var bullets = alien.bullets;
+	      var spaceship = stage.getChildByName("spaceship");
+	      if (bullets.length > 0) {
 	        for (var i = 0; i < bullets.length; i++) {
-	          bullets[i].y -= 15;
+	          if (bullets[i].y + bullets[i].height >= spaceship.y && bullets[i].x <= spaceship.x + spaceship.width && bullets[i].x + bullets[i].width >= spaceship.x) {
+	            //hit
+	            (0, _helper.updateLives)();
+	            alien.bullets.splice(i, 1);
+	            stage.removeChild(bullets[i]);
+	            spaceship.x = canvas.width / 2 - 15;
+	            spaceship.y = canvas.height - 30;
+	          }
 	        }
 	      }
 	    }
@@ -410,6 +510,41 @@
 	};
 	
 	exports.default = Background;
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var incrementScore = function incrementScore() {
+	  var score = $('.score').html();
+	  var newScore = parseInt(score) + 100;
+	  $('.score').html(newScore.toString());
+	};
+	
+	// const newWave = (stage) => {
+	//   stage.
+	// }
+	
+	var gotHit = function gotHit() {};
+	
+	var updateLives = function updateLives() {
+	  var lives = $('.lives').html();
+	  var newLives = parseInt(lives) - 1;
+	  if (newLives === 0) {
+	    //game over
+	  } else {
+	    debugger;
+	    $('.lives').html(newLives.toString());
+	  }
+	};
+	
+	exports.incrementScore = incrementScore;
+	exports.updateLives = updateLives;
 
 /***/ }
 /******/ ]);
